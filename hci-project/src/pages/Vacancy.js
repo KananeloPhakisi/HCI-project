fdef9e5efc4983c5ccff9a47246ca16a696ce248
Maverickk <73136742+Maverick-Stan@users.noreply.github.com>
2022-01-26 03:39:55 +0200

    Add files via upload

--- hci-project/src/pages/Vacancy.js
+++ hci-project/src/pages/Vacancy.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import "./mpages.css";
 
 const Vacancy = () => {
   return (
     <div className='home'>
-      <h1>Vacancy</h1>
+      <div className='card' >
+            <div className='cardBody'>
+                <img className='cardImage' aria-orientation='top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/267px-PDF_file_icon.svg.png' alt='Tender Document'></img>
+                <p className='cardDescrip'>Ministry of Communications External Circular Notice No 3.</p>
+                <button className='cardButton' href=''>Download</button>
+
+            </div>
+          </div>
+        <div className='card' >
+            <div className='cardBody'>
+                <img className='cardImage' aria-orientation='top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/267px-PDF_file_icon.svg.png' alt='Tender Document'></img>
+                <p className='cardDescrip'>Ministry of Communications, Science and Technology Vacancies</p>
+                <button className='cardButton' href=''>Download</button>
+
+            </div>
+        </div>
+        <div className='card' >
+            <div className='cardBody'>
+                <img className='cardImage' aria-orientation='top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/267px-PDF_file_icon.svg.png' alt='Tender Document'></img>
+                <p className='cardDescrip'>Ministry of Communications, Science and Technology external circular notice No.8 of 2021</p>
+                <button className='cardButton' href=''>Download</button>
+
+            </div>
+        </div>
+        <div className='card' >
+            <div className='cardBody'>
+                <img className='cardImage' aria-orientation='top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/267px-PDF_file_icon.svg.png' alt='Tender Document'></img>
+                <p className='cardDescrip'>Expression of Interest-Project Coordinator- 09-11-2021</p>
+                <button className='cardButton' href=''>Download</button>
+
+            </div>
+        </div>
+        <div className='card' >
+            <div className='cardBody'>
+                <img className='cardImage' aria-orientation='top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/267px-PDF_file_icon.svg.png' alt='Tender Document'></img>
+                <p className='cardDescrip'>Ministry of Communications, Science and Technology internal circular notice No.10 of 2021</p>
+                <button className='cardButton' href=''>Download</button>
+
+            </div>
+          </div>
+        
     </div>
   );
 };
